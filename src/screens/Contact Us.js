6fbd080de52ac0afb975b41692166d62335832c4
Resources - DTW <resource.dtw@gmail.com>
2025-02-20 13:03:20 +0530

    fix!:map embed

--- src/screens/Contact Us.js
+++ src/screens/Contact Us.js
@@ -367,6 +367,20 @@ export default function ContactUs() {
           </div>
         </div>
       </div>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7774.042003263787!2d80.2253517508507!3d13.034334527370943!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52675c20776c0d%3A0x1cb53c7de4f2091d!2sBiz%20Craft%20Creative%20Services!5e0!3m2!1sen!2sin!4v1740036116064!5m2!1sen!2sin"
+        width="100%"
+        height="400px"
+        style={{
+          borderTopLeftRadius: 80,
+          borderTopRightRadius: 80,
+        }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+
       <Footer />
     </div>
   );
